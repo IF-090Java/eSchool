e2d712c46790bdf2cbcd8fd5ca3625c7dccf61a3
Vitaliy_Popovych <popovichvtaljj@gmail.com>
2018-10-05 12:39:25 +0300

    Docker fix

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker run --name ejournal_mysql -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=ejournal_mysql -d mysql:5.6
-docker build -f Dockerfile -t docker-ejournal:1.0
-docker run -p 8080:8080 docker-ejournal
+docker run --name ejournal_mysql -e MYSQL_ROOT_PASSWORD=root -e MYSQL_USER=admin -e MYSQL_PASSWORD=Admin_Pass01 -e MYSQL_DATABASE=ejournal -d mysql:5.6
+docker build -f Dockerfile -t docker-ejournal:1.0 .
+docker run --name ejournal -p 8080:8080 --link ejournal_mysql:mysql docker-ejournal:1.0
