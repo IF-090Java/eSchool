8bc1ba7dd72b28b43b815774cfd9214fb2f251d8
ruslan <crashtime078@gmail.com>
2018-10-14 17:23:41 +0300

    added swagger annotations

--- src/main/java/academy/softserve/eschool/controller/HomeworkController.java
+++ src/main/java/academy/softserve/eschool/controller/HomeworkController.java
@@ -1,6 +1,10 @@
 package academy.softserve.eschool.controller;
 
 import academy.softserve.eschool.dto.Homework;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +15,18 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/homeworks")
+@Api(value = "Homework's Endpoint", description = "Get homeworks")
 public class HomeworkController {
+
     @GetMapping("/subjects/{id}/classes/{id}")
+    @ApiOperation(value = "Get homeworks by subjects and classes")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(code = 200, message = "OK"),
+                    @ApiResponse(code = 204, message = "No content"),
+                    @ApiResponse(code = 500, message = "Serever error")
+            }
+    )
     public List<Homework> getHomeworks(){
         List<Homework> list = new ArrayList<>();
         list.add(new Homework(new Date(),"p.125","file1.txt"));
--- src/main/java/academy/softserve/eschool/controller/JournalController.java
+++ src/main/java/academy/softserve/eschool/controller/JournalController.java
@@ -1,6 +1,10 @@
 package academy.softserve.eschool.controller;
 
 import academy.softserve.eschool.dto.Journal;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +13,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@Api(value = "Journal's Endpoint", description = "Get journals")
 @RequestMapping("/journals")
 public class JournalController {
+
+    @ApiOperation(value = "Get list of journals")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(code = 200, message = "OK"),
+                    @ApiResponse(code = 204, message = "No content"),
+                    @ApiResponse(code = 500, message = "Serever error")
+            }
+    )
     @GetMapping("")
     public List<Journal> getJournals(){
         List<Journal> list = new ArrayList<>();
@@ -22,6 +36,14 @@ public class JournalController {
         return list;
     }
 
+    @ApiResponses(
+            value = {
+                    @ApiResponse(code = 200, message = "OK"),
+                    @ApiResponse(code = 204, message = "No content"),
+                    @ApiResponse(code = 500, message = "Serever error")
+            }
+    )
+    @ApiOperation(value = "Get journal by subjects and classes")
     @GetMapping("/subjects/{id}/classess/{id}")
     public void getJournalsTable(){
 
