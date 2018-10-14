7e4ac46cc5a82da5d09c4a702e61a9b56393389f
Vitaliy_Popovych <popovichvtaljj@gmail.com>
2018-10-14 14:08:31 +0300

    Added DTO, ClassController

---
+++ src/main/java/academy/softserve/eschool/dto/ClassDTO.java
@@ -0,0 +1,20 @@
+package academy.softserve.eschool.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
+
+@ApiModel
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClassDTO {
+    @ApiModelProperty(required = true, notes = "Class year, from 1 to 12")
+    private int classYear;
+
+    @ApiModelProperty(required = false, notes = "Class name, like Б, А. Can be empty")
+    private String className;
+
+    @ApiModelProperty(required = false, notes = "Some additional information. Can be empty")
+    private String classDescription;
+}
