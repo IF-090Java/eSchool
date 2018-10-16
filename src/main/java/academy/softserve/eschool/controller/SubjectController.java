0c0cb3a81f73a42169815229ce09826ae7102744
Ihor-Kudiarskyi <Igor_kudiarskyy@ukr.net>
2018-10-17 01:03:04 +0300

    Create classes SubjectDTO, SubjectController and HTML-pages

---
+++ src/main/java/academy/softserve/eschool/controller/SubjectController.java
@@ -0,0 +1,68 @@
+package academy.softserve.eschool.controller;
+
+import academy.softserve.eschool.dto.SubjectDTO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/subjects")
+@Api(value = "subjects", description = "API endpoints for subjects")
+public class SubjectController {
+	
+	@ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 500, message = "Internal Server Error")
+    })
+    @ApiOperation(value = "Get all subjects", response = SubjectDTO.class)
+    @GetMapping()
+    public List<SubjectDTO> getAll(){
+        List<SubjectDTO> list = new ArrayList<>();
+        list.add(new SubjectDTO(1, "Історія України"));
+        list.add(new SubjectDTO(2, "Інформатика"));
+        list.add(new SubjectDTO(3, "Англійська мова"));
+        list.add(new SubjectDTO(4, "Українська мова"));
+        list.add(new SubjectDTO(5, "Українська література"));
+        list.add(new SubjectDTO(6, "Фізика"));
+        list.add(new SubjectDTO(7, "Географія"));
+        list.add(new SubjectDTO(8, "Біологія"));
+        list.add(new SubjectDTO(9, "Математика"));
+        list.add(new SubjectDTO(10, "Хімія"));
+        return list;
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Successfully created"),
+            @ApiResponse(code = 500, message = "Internal Server Error")
+    })
+    @ApiOperation(value = "Add new subject")
+    @PostMapping
+    public SubjectDTO addSubject(@RequestBody SubjectDTO newSubject){
+        return newSubject;
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 500, message = "Internal Server Error")
+    })
+    @ApiOperation(value = "Get a subject by Id", response = SubjectDTO.class)
+    @GetMapping("/{id}")
+    public SubjectDTO getSubjectById(@PathVariable String id){
+        return new SubjectDTO(1, "Історія України");
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(code = 500, message = "Internal Server Error")
+    })
+    @ApiOperation("Edit a subject")
+    @PutMapping("/{id}")
+    public SubjectDTO editSubjectClass(@PathVariable String id, @RequestBody SubjectDTO editSubject){
+        return editSubject;
+    }
+}
