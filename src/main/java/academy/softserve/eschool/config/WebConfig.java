ffd3ec4b03df510fc73b50b65e4f8ff72ab6e418
Vitaliy_Popovych <popovichvtaljj@gmail.com>
2018-10-17 20:06:18 +0300

    Bugs fixed, WebConfig and PageMappingController added

---
+++ src/main/java/academy/softserve/eschool/config/WebConfig.java
@@ -0,0 +1,27 @@
+package academy.softserve.eschool.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        super.addResourceHandlers(registry);
+    }
+
+    @Bean
+    public ViewResolver viewResolver(){
+        UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
+        viewResolver.setViewClass(InternalResourceView.class);
+        return viewResolver;
+    }
+}
---
+++ src/main/java/academy/softserve/eschool/controller/PageMappingController.java
@@ -0,0 +1,13 @@
+package academy.softserve.eschool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageMappingController {
+    @RequestMapping("classes/{id}/edit")
+    public String editClassPage(){
+        return "/static/editClass.html";
+    }
+}
+
