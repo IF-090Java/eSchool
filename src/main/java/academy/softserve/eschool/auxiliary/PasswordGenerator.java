68bf9b242b5d3be6f6bafc7d3313938041b72748
vHotsuliak <vasyl.hotsuliak@gmail.com>
2018-10-23 13:41:37 +0300

    created generators

---
+++ src/main/java/academy/softserve/eschool/auxiliary/PasswordGenerator.java
@@ -0,0 +1,28 @@
+package academy.softserve.eschool.auxiliary;
+
+import java.security.SecureRandom;
+
+public class PasswordGenerator {
+    private static SecureRandom random = new SecureRandom();
+
+    /** different dictionaries used */
+    private static final String ALPHA_CAPS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String ALPHA = "abcdefghijklmnopqrstuvwxyz";
+    private static final String NUMERIC = "0123456789";
+    private static final String SPECIAL_CHARS = "!@#$%^&*_=+-/";
+
+    /**
+     * Method will generate random string based on the parameters
+     * @param len the length of the random string
+     * @param dic the dictionary used to generate the password
+     * @return the random password
+     */
+    public static String generatePassword(int len, String dic) {
+        String result = "";
+        for (int i = 0; i < len; i++) {
+            int index = random.nextInt(dic.length());
+            result += dic.charAt(index);
+        }
+        return result;
+    }
+}
