a8b417f27b99f3e9953cea94353c628d379aeb60
Serhii Boiko <sergiy1300@gmail.com>
2018-10-24 12:04:07 +0300

    fix

--- src/main/java/academy/softserve/eschool/repository/MarkRepository.java
+++ src/main/java/academy/softserve/eschool/repository/MarkRepository.java
@@ -12,13 +12,14 @@ import academy.softserve.eschool.model.Mark;
 @Repository
 public interface MarkRepository extends JpaRepository<Mark, Integer> {
 	
-	@Query(value="select m.mark, l.date "
+	@Query(value="select AVG(m.mark), l.date "
 			+ "from mark m left join lesson l on m.lesson_id = l.id "
 			+ "where (:subjectId is null or l.subject_id = :subjectId)"
 			+ "and (:classId is null or l.clazz_id = :classId)"
 			+ "and (:studentId is null or m.student_id = :studentId)"
 			+ "and (:startDate is null or l.date >= :startDate)"
-			+ "and (:endDate is null or l.date <= :endDate)", nativeQuery=true)
+			+ "and (:endDate is null or l.date <= :endDate) "
+			+ "group by l.date", nativeQuery=true)
 	public List<Object[]> findAll(@Param("subjectId") Integer subjectId, @Param("classId")Integer classId,
 			@Param("studentId") Integer studentId, @Param("startDate") String startDate, @Param("endDate") String endDate);
 	
