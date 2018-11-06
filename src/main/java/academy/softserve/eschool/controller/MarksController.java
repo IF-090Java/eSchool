7ae10656aa6eabd9db055d59bd816286eae161c2
Serhii Boiko <sergiy1300@gmail.com>
2018-11-06 13:27:30 +0200

    added missing import

--- src/main/java/academy/softserve/eschool/controller/MarksController.java
+++ src/main/java/academy/softserve/eschool/controller/MarksController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
