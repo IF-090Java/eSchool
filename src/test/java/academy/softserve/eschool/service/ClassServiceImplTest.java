7ca2710f569a8b5d1fd496b599b82dc0dd6d0f59
popovych <popovichvtaljj@gmail.com>
2018-11-21 00:40:13 +0200

    Test folder initialization

---
+++ src/test/java/academy/softserve/eschool/service/ClassServiceImplTest.java
@@ -0,0 +1,9 @@
+package academy.softserve.eschool.service;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ClassServiceImplTest {
+    
+}
