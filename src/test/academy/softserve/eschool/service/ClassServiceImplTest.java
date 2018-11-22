b54bb13fce81d9c0f91c2169f5114dd628ee71fd
Pavlo Darchyn <pablodarchunn@gmail.com>
2018-11-23 00:23:20 +0200

    project structure fixed

--- src/test/academy/softserve/eschool/service/ClassServiceImplTest.java
+++
@@ -1,9 +0,0 @@
-package academy.softserve.eschool.service;
-
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
-
-@RunWith(MockitoJUnitRunner.class)
-public class ClassServiceImplTest {
-
-}
