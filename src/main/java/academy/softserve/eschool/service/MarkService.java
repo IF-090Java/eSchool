93f0c94aacf262ea6eda3be7bd93e1ebac13979d
ruslan <crashtime078@gmail.com>
2018-11-28 16:36:16 +0200

    changed and added new logs

--- src/main/java/academy/softserve/eschool/service/MarkService.java
+++ src/main/java/academy/softserve/eschool/service/MarkService.java
@@ -71,6 +71,7 @@ public class MarkService implements MarkServiceBase {
 
     @Override
     public MarkDTO saveMark(MarkDTO dto) {
+        logger.info("Adding mark lesson[id={}],student[id={}",dto.getIdLesson(),dto.getIdStudent());
         markRepo.saveMarkByLesson(dto.getIdStudent(),dto.getIdLesson(),dto.getMark(),dto.getNote());
         Mark mark = markRepo.findTopByStudentIdAndLessonId(dto.getIdStudent(),dto.getIdLesson());
         MarkDTO markDTO = MarkDTO.builder()
@@ -80,13 +81,12 @@ public class MarkService implements MarkServiceBase {
                 .idStudent(mark.getStudent().getId())
                 .note(mark.getNote())
                 .build();
-        logger.info("Added mark for lesson[id="+dto.getIdLesson()+"], student[id="+dto.getIdStudent()+"]");
         return markDTO;
     }
 
     @Override
     public void updateType(int idLesson, String markType) {
+        logger.info("Editing markType[{}] lesson[id={}]",markType,idLesson);
         markRepo.saveTypeByLesson(idLesson,markType);
-        logger.info("Edited mark type for lesson[id="+idLesson+"], newType = "+markType);
     }
 }
