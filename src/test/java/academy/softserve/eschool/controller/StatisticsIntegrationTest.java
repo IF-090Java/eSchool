d19f8b5a26846f277158f1427eec51b69c22cf06
Serhii Boiko <sergiy1300@gmail.com>
2018-11-28 21:40:26 +0200

    mark integration test renamed

--- src/test/java/academy/softserve/eschool/controller/StatisticsIntegrationTest.java
+++ src/test/java/academy/softserve/eschool/controller/StatisticsIntegrationTest.java
@@ -9,7 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-public class MarkControllerIntegrationTest extends ControllerIntegrationTestBase {
+public class StatisticsIntegrationTest extends ControllerIntegrationTestBase {
 
     private String token;
     private HttpHeaders headers;
