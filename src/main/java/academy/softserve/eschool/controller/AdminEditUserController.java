a00900b1ac3f9343bd79bafe73aa103b15720c17
vHotsuliak <vasyl.hotsuliak@gmail.com>
2018-12-07 11:42:00 +0200

    add docs to controllers

--- src/main/java/academy/softserve/eschool/controller/AdminEditUserController.java
+++ src/main/java/academy/softserve/eschool/controller/AdminEditUserController.java
@@ -12,6 +12,8 @@ import academy.softserve.eschool.wrapper.Status;
 import io.swagger.annotations.*;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +23,8 @@ import org.springframework.web.bind.annotation.*;
 @Api(description = "Operations with changing user data for the admin.")
 @RequiredArgsConstructor
 public class AdminEditUserController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TeacherController.class);
 
     @NonNull
     private StudentRepository studentRepository;
@@ -34,6 +38,15 @@ public class AdminEditUserController {
     @NonNull
     private TeacherService teacherService;
 
+    /**
+     * Admin updates the profile of the pupil by passing {@linc EditUserDTO} and ID of the pupil.
+     * Only admin has access to this endpoint.
+     * The admin is allowed to update every account.
+     * @param student User object
+     * @param idStudent ID of the pupil.
+     * @return Updated student as {@link User} object
+     *          in {@link GeneralResponseWrapper} with http status code
+     */
     @ApiOperation(value = "Admin updates the profile of the pupil", extensions = {@Extension(name = "roles", properties = {
             @ExtensionProperty(name = "admin", value = "the admin is allowed to update every account")})})
     @ApiResponses(
@@ -48,10 +61,20 @@ public class AdminEditUserController {
     public GeneralResponseWrapper<User> updateStudent(
             @ApiParam(value = "User object.", required = true) @RequestBody EditUserDTO student,
             @ApiParam(value = "ID of the pupil.", required = true) @PathVariable int idStudent){
+        LOGGER.info("Updating student [{} {}]", student.getLastname(), student.getFirstname());
         return new GeneralResponseWrapper<>(Status.of(HttpStatus.OK) , studentService.adminUpdateStudent(studentRepository.findById(idStudent).get(), student));
 
     }
 
+    /**
+     * Admin updates the profile of the teacher by passing {@linc EditUserDTO} and ID of the teacher.
+     * Only admin has access to this endpoint.
+     * The admin is allowed to update every account.
+     * @param teacher User object
+     * @param idTeacher ID of the teacher.
+     * @return Updated teacher as {@link TeacherDTO} object
+     *          in {@link GeneralResponseWrapper} with http status code.
+     */
     @ApiOperation(value = "Admin updates the profile of the teacher.", extensions = {@Extension(name = "roles", properties = {
             @ExtensionProperty(name = "admin", value = "the admin is allowed to update every account")})})
     @ApiResponses(
@@ -66,6 +89,7 @@ public class AdminEditUserController {
     public GeneralResponseWrapper<TeacherDTO> updateTeacher(
             @ApiParam(value = "User object.", required = true) @RequestBody EditUserDTO teacher,
             @ApiParam(value = "ID of the teacher.", required = true) @PathVariable int idTeacher){
+        LOGGER.info("Updating teacher [{} {}]", teacher.getLastname(), teacher.getFirstname() + "created");
         return new GeneralResponseWrapper<>(Status.of(HttpStatus.OK), teacherService.adminUpdateTeacher(userRepository.findById(idTeacher).get(), teacher));
     }
 }
